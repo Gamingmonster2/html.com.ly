a3c0bc823bc2521583742ea7e4f23e1f20fcc1d9
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-07-11 12:54:13 +0200

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Cairo", "Space Grotesk", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"],
+      }
+    },
+  },
+  plugins: [],
+}
